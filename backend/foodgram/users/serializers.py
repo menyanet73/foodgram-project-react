ad0029afb6397d9e8ee1d0c2b333fd2b94187403
Maxim <menyanet73@gmail.com>
2022-05-26 13:54:46 +0300

    serializer create started

--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -1,3 +1,11 @@
+from rest_framework import serializers
+from users.models import User
 
 
-SignUpUserSerializer()
+class SignUpUserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'first_name', 'last_name', 'password')
+
+    def validate_username(self, username):
